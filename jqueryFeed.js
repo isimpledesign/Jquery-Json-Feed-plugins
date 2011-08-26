14dd244eb0b8c85cbd27f26d9a7f9d22281739ff
sam <info@isimpledesign.co.uk>
2011-08-26 12:14:04 +0100

    stuck on max height and working out stoppping the scroll

--- jqueryFeed.js
+++ jqueryFeed.js
@@ -7,7 +7,7 @@
 				feedUrl: 'http://gdata.youtube.com/feeds/api/videos?v=2&alt=jsonc&q=liverpool&max-results=24&orderby=published&time=today', // the url to the feed you need to pul in
 				axis: 'vertical', // the axis horizontal or vertical
                 scrollWidth: 300, // the scroll width
-                scrollHeight: 60, // the scroll height
+                scrollHeight: 100, // the scroll height
                 speed: "slow", // scroll speed
 				items_per_page: 4, // number off items per page
 				navClass: "MFSN",
@@ -25,57 +25,90 @@
 						 console.log("json"+json.data.items.length);
 						 
 						 var max_length = json.data.items.length;
-						 var max_elem   = max_length / options.items_per_page;
-						 
-						 //console.log(max_elem);
-						  var html = "<ul id='scroll'>";
+						 var max_elem   = max_length / options.items_per_page - 1;
+						 console.log(max_elem);
+						 var height = options.scrollHeight * options.items_per_page;
+						 console.log(height);
+					     var html = '';
 						 $.each(json.data.items, function(i, feed) {
 						 
-						 console.log(feed);
+						 //console.log(feed);
 						 
 						 // setup feed vars here
 						 var thumbnail = feed.thumbnail.sqDefault;
 						 
-						 html += '<li><img src="'+(thumbnail)+'" width="100" height="60"/></li>'; 
+						 html += '<li style="height:'+options.scrollHeight+'px;"><img src="'+(thumbnail)+'" width="100" height="60"/></li>'; 
 						 
 						 });
-						html += "</ul>";
-						$('#'+options.scrollId).html(html).fadeIn(2000);
+						 
+						 // add css to container
+						 var cssObj = {
+						  'height' : height+'px',
+						  'overflow' : 'hidden',
+						  'position' : 'relative',
+						  'color' : 'rgb(0,40,244)'
+						}
+
+						$('#'+options.scrollId).html("<ul style='position:absolute;'>"+html+"</ul>").css(cssObj);
+						
+						$('#'+options.scrollId).after("<a href='#' id='up'>up</a><span>"+max_elem+"</span><a href='#' id='down'>down</a>");
+						
+						// up arrow sroll
+						$('#up').click(function(){
+						
+						var position = $("ul").position();
+
+						var scroll = position.top - height;
+						
+						var overallHeight = options.items_per_page * height;
+						
+						var test = parseFloat(position.top) + 400;
+						
+						console.log(test);
+						
+						console.log(position.top,overallHeight);
+						
+						if(position.top >= '-'+overallHeight){
+						 if($("ul:animated").length==0){
+						 $("ul").stop().animate({ 'top': scroll + 'px' }, options.speed );
+						 }
+						}
+						
+						return false;
+						
+						});
+						
+						// down arrow scroll
+						$('#down').click(function(){
+			            
+						var position = $("ul").position();
+						
+						var scroll = position.top + height;
+						
+						console.log(position);
+						
+						if(position.top != 0){
+						 if($("ul:animated").length==0){
+						 $("ul").stop().animate({ 'top': scroll + 'px' }, options.speed );
+						}
+						}
+						
+						return false;
+						
+						});
 						
 					});
-
+					
+					
+					
 					return false;
 					 
 			}
 			
-			getFeed();
+			
 
 			
-			
-			
-			/**
-				 * Calculate the maximum number of pages
-				 */
-				function numPages() {
-					return Math.ceil(maxentries/options.items_per_page);
-				} 
-             
-
-
-            return this.each(function() {
-			
-			     
-				 //console.log($(this)); 
-                 
-				 
-				 
-				
-				 
-				 
-				 
-                  
-                  
-            });
+			getFeed()
 			
 			
 			
